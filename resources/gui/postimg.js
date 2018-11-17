b5282d526d5fd8f23bd462309d44afac8d570951
gdeachaval <gonzaachaval98@gmail.com>
2018-11-17 15:53:34 -0300

    Minor JS CORS changes

--- resources/gui/postimg.js
+++ resources/gui/postimg.js
@@ -17,10 +17,6 @@ http.onreadystatechange = function () {
 
 function postImgB64(element) {
     http.open("POST", url, true);
-    http.withCredentials = true;
-    http.setRequestHeader("Content-Type", "application/json");
-    http.setRequestHeader("Access-Control-Allow-Origin", "http://localhost:9000/image");
-    http.setRequestHeader("Access-Control-Allow-Methods", "POST");
     init();
     const file = element.files[0];
     let reader = new FileReader();
